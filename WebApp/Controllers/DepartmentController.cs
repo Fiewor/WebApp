920e4d213793bc1040c0f4fd1ee59d2d916c0be0
Fiewor <johnfiewor@gmail.com>
2023-04-17 11:32:41 +0100

    Add project files.

---
+++ WebApp/Controllers/DepartmentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        //to read the connection strings from appsetttings file, we need to make use of dependency injection
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // now, an API method to get all data from department table
+        [HttpGet]
+        public JsonResult Get()
+        {
+            // currently writing raw queries, but other options are 'stored procedures' and 'entity framework'
+            string query = @"select DepartmentId, DepartmentName from dbo.Department";
+            // get data into a DataTable object
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
